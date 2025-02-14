bd9ab0f93ab357b3b7431833a4ed17ca4dcaa939
spshamim <spshamim90@gmail.com>
2025-02-14 20:32:08 +0600

    Finalised

--- config/db.js
+++ config/db.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected successfully on ATLAS.");
     } catch (error) {
-        console.error("MongoDB connection error:", error.message);
         process.exit(1);
     }
 };
